54aa09da3c4f01acd94a89c648f7c072ff31b9b3
woshijinlei <1067852064@qq.com>
2022-03-21 11:52:30 +0800

    home

--- src/main/java/com/simple/commonutils/package/PackageConst.kt
+++ src/main/java/com/simple/commonutils/package/PackageConst.kt
@@ -3,7 +3,9 @@ package com.simple.commonutils.`package`
 object PackageConst {
     const val TwitterApp = "com.twitter.android"
     const val FaceBookApp = "com.facebook.katana"
+    const val YouTube = "com.google.android.youtube"
     const val WhatsApp = "com.whatsapp"
+    const val Snapchat = "com.snapchat.android"
     const val InstagramApp = "com.instagram.android"
     const val TikTokMusicApp = "com.zhiliaoapp.musically"
     const val TikTokTrillApp = "com.ss.android.ugc.trill"
