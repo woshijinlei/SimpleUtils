ad20189be4e429984237eedd974423c3c9276957
Tom <1067852064@qq.com>
2025-03-25 15:12:27 +0800

    work

--- src/main/java/com/simple/commonutils/Log.kt
+++ src/main/java/com/simple/commonutils/Log.kt
@@ -3,23 +3,23 @@ package com.simple.commonutils
 import android.util.Log
 
 fun Any?.log(tag: Any?, msg: Any? = null) {
-    if (BuildConfig.DEBUG) {
-        val clazzTag = if (this == null) {
-            "null."
-        } else {
-            this::class.java.simpleName + "."
-        }
-        Log.d("woshijinlei", "${clazzTag}${tag}:$msg")
+//    if (BuildConfig.DEBUG) {
+    val clazzTag = if (this == null) {
+        "null."
+    } else {
+        this::class.java.simpleName + "."
     }
+    Log.d("woshijinlei", "${clazzTag}${tag}:$msg")
+//    }
 }
 
 fun Any?.logE(tag: Any?, msg: Any? = null) {
-    if (BuildConfig.DEBUG) {
-        val clazzTag = if (this == null) {
-            "null."
-        } else {
-            this::class.java.simpleName + "."
-        }
-        Log.e("woshijinlei", "${clazzTag}${tag}:$msg")
+//    if (BuildConfig.DEBUG) {
+    val clazzTag = if (this == null) {
+        "null."
+    } else {
+        this::class.java.simpleName + "."
     }
+    Log.e("woshijinlei", "${clazzTag}${tag}:$msg")
+//    }
 }
